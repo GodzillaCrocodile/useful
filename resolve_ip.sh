621420d5afccf361dc354f0e74380f8066f3a62e
Evgeny Stroev <estroev@alfabank.ru>
2017-10-30 19:17:46 +0300

    Delete bash scripts

--- resolve_ip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'DOMAINS=($(cat domains.txt))'
-for line in "${DOMAINS[@]}"; do
-	ip=$(getent hosts "$line" | awk '{ print $1 }')
-	echo $ip, "$line"
-done
