5fa78ba1085c1609bd47def7bd298f246d2db335
Evgeny Stroev <estroev@alfabank.ru>
2017-11-17 18:51:17 +0300

    Refactoring

--- bash/resolve_ip.sh
+++ bash/resolve_ip.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'DOMAINS=($(cat domains.txt))'
+INPUT_FILE=$1
+OUTPUT_FILE=$2
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'DOMAINS=($(cat $1))'
 for line in "${DOMAINS[@]}"; do
-	ip=$(getent hosts "$line" | awk '{ print $1 }')
-	echo $ip, "$line"
+	ips=$(getent hosts "$line" | awk '{ print $1 }')
+    if [ -z "$ips" ]
+        then
+            echo $ips,"$line" >> $2
+        else
+            for ip in $ips
+                do
+                    echo $ip,"$line" >> $2
+            done
+    fi
 done
